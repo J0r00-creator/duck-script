a3c3082ee7f3ae9b029d826d2c959c9bccd471ee
J0r00-creator <jack.mck.04@gmail.com>
2025-10-14 15:33:04 -0400

    Update run_wallpaper_installer.bat

--- run_wallpaper_installer.bat
+++ run_wallpaper_installer.bat
@@ -1,4 +1,5 @@
 @echo off
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/J0r00-creator/ducky-script/main/install_wallpaper_sched.ps1' -OutFile $env:USERPROFILE\Desktop\install_wallpaper_sched.ps1"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/J0r00-creator/duck-script/main/install_wallpaper_sched.ps1' -OutFile $env:USERPROFILE\Desktop\install_wallpaper_sched.ps1"
 powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%USERPROFILE%\Desktop\install_wallpaper_sched.ps1"
+
 echo Done > "%USERPROFILE%\Desktop\wallpaper_installer_done.txt"
